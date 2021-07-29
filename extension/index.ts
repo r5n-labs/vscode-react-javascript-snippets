fb43931e3c67896fb900ea59e520e4f6f86cc891
Damian Sznajder <damsznajder@gmail.com>
2021-07-29 20:25:38 +0200

    chore: update extension packages

--- extension/index.ts
+++ extension/index.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
 const jsSnippets = require('../snippets/snippets.json')
@@ -14,60 +12,53 @@ type Snippet = {
 const convertSnippetArrayToString = (snippetArray: Array<string>): string =>
   snippetArray.join('\n')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "snippet-search" is now active!')
+  const {
+    commands: { registerCommand },
+    window: { showQuickPick, activeTextEditor },
+  } = vscode
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'extension.snippetSearch',
-    async () => {
-      const javascriptSnippets = Object.entries(jsSnippets as Array<Snippet>)
-      const typescriptSnippets = Object.entries(tsSnippets as Array<Snippet>)
-      const snippetsArray: Array<[string, Snippet]> = javascriptSnippets.concat(
-        typescriptSnippets
-      )
+  const disposable = registerCommand('extension.snippetSearch', async () => {
+    const javascriptSnippets = Object.entries(jsSnippets as Array<Snippet>)
+    const typescriptSnippets = Object.entries(tsSnippets as Array<Snippet>)
+    const snippetsArray: Array<[string, Snippet]> =
+      javascriptSnippets.concat(typescriptSnippets)
 
-      const items = snippetsArray.map(
-        ([shortDescription, { prefix, body, description }], index) => {
-          const value = typeof prefix === 'string' ? prefix : prefix[0]
+    const items = snippetsArray.map(
+      ([shortDescription, { prefix, body, description }], index) => {
+        const value = typeof prefix === 'string' ? prefix : prefix[0]
 
-          return {
-            id: index,
-            description: description || shortDescription,
-            label: value,
-            value,
-            body,
-          }
+        return {
+          id: index,
+          description: description || shortDescription,
+          label: value,
+          value,
+          body,
         }
-      )
-
-      const options = {
-        matchOnDescription: true,
-        matchOnDetail: true,
-        placeHolder: 'Search snippet',
       }
+    )
 
-      const snippet = (await vscode.window.showQuickPick(items, options)) || {
-        body: '',
-      }
-      const activeTextEditor = vscode.window.activeTextEditor
-      const body =
-        typeof snippet.body === 'string'
-          ? snippet.body
-          : convertSnippetArrayToString(snippet.body)
-      activeTextEditor &&
-        activeTextEditor.insertSnippet(new vscode.SnippetString(body))
+    const options = {
+      matchOnDescription: true,
+      matchOnDetail: true,
+      placeHolder: 'Search snippet',
     }
-  )
+
+    const snippet = (await showQuickPick(items, options)) || {
+      body: '',
+    }
+
+    const body =
+      typeof snippet.body === 'string'
+        ? snippet.body
+        : convertSnippetArrayToString(snippet.body)
+
+    if (activeTextEditor) {
+      activeTextEditor.insertSnippet(new vscode.SnippetString(body))
+    }
+  })
 
   context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
